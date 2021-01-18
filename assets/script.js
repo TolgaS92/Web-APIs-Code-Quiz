75e6f0f5fee83e8b581f5c1aa9eaccddf56b2cc1
Tolga Secme <tolgasecme@icloud.com>
2021-01-18 13:21:19 -0700

    Header and start button added

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+//Create a random questions
+//
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 60;
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
+
+    }, 1000);
+}
+
+var startButton = document.querySelector(".start")
+
+startButton.addEventListener("click", function () {
+    setTime();
+    document.getElementById("toggle").style.visibility = "hidden";
+});
+
+var myQuest = ["Inside which HTML element do we put the JavaScript?", "What is the correct JavaScript syntax to change the content of the HTML element below?<p id='demo'>This is a demonstration.</p>"]
+
+function questions(k) {
+    console.log(k);
+}
+questions(myQuest);
